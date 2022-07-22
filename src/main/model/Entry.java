ee8a1fe6735ad93786b3e2f5aee23a9edbadaac3
wgao23 <wgao23@students.cs.ubc.ca>
2022-07-21 23:21:30 -0700

    Completed implementation for Entry and Journal

--- src/main/model/Entry.java
+++ src/main/model/Entry.java
@@ -2,45 +2,50 @@ package model;
 
 //Represents a journal entry with content, an ID number, and a mood
 public class Entry {
+    private String content;
+    private int idNumber;
+    private MoodType mood;
 
     //EFFECTS: constructs a new entry with given content, ID number, and mood type
     public Entry(String content, int id, MoodType mood) {
-        //stub
+        setContent(content);
+        setIdNumber(id);
+        setMood(mood);
     }
 
     //REQUIRES: content != "", content length <= CONTENT_CHARACTER_LIMIT
     //MODIFIES: this
     //EFFECTS: change the entry's content to the given string
     public void setContent(String content) {
-        //stub
+        this.content = content;
     }
 
     //REQUIRES: id >= 0
     //MODIFIES: this
     //EFFECTS: change the entry's ID number to the given ID number
     public void setIdNumber(int id) {
-        //stub
+        this.idNumber = id;
     }
 
     //REQUIRES: mood != null
     //MODIFIES: this
     //EFFECTS: change the entry's mood type to the given mood
     public void setMood(MoodType mood) {
-        //stub
+        this.mood = mood;
     }
 
     //EFFECTS: return the content of the entry
     public String getContent() {
-        return ""; //stub
+        return content;
     }
 
     //EFFECTS: return the ID number of the entry
     public int getIdNumber() {
-        return 0; //stub
+        return idNumber;
     }
 
     //EFFECTS: return the mood category of the entry
     public MoodType getMood() {
-        return null;
+        return mood;
     }
 }
--- src/main/model/Journal.java
+++ src/main/model/Journal.java
@@ -1,13 +1,17 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static model.MoodType.*;
 
 //Represents a mood journal with entries organized by emotion
 public class Journal {
+    private List<Entry> journalEntries;
 
     //EFFECTS: constructs a new journal with no entries
     public Journal() {
-        //stub
+        journalEntries = new ArrayList<>();
     }
 
     //MODIFIES: this
@@ -15,25 +19,43 @@ public class Journal {
     //add given entry to the journal and return true.
     //Otherwise, do nothing and return false.
     public boolean addEntry(Entry entry) {
-        return false; //stub
+        for (Entry e : journalEntries) {
+            if (e.getIdNumber() == entry.getIdNumber()) {
+                return false;
+            }
+        }
+        journalEntries.add(entry);
+        return true;
     }
 
     //REQUIRES: an entry with the given ID exists in the journal
     //MODIFIES: this
     //EFFECTS: remove the entry with the given ID from the journal
     public void removeEntry(int id) {
-        //stub
+        Entry entryToRemove = new Entry("", 111, HAPPY);
+        for (Entry e : journalEntries) {
+            if (e.getIdNumber() == id) {
+                entryToRemove = e;
+            }
+        }
+        journalEntries.remove(entryToRemove);
     }
 
     //REQUIRES: there is at least one entry of the given mood type in the journal
     //EFFECTS: returns all entries in the journal of the given mood type
     public List<Entry> getEntriesOfMoodType(MoodType mood) {
-        return null; //stub
+        List<Entry> entriesOfSpecificMood = new ArrayList<>();
+        for (Entry e : journalEntries) {
+            if (e.getMood().equals(mood)) {
+                entriesOfSpecificMood.add(e);
+            }
+        }
+        return entriesOfSpecificMood;
     }
 
     //EFFECTS: returns all entries in the journal
     public List<Entry> getEntries() {
-        return null; //stub
+        return journalEntries; //stub
     }
 
 }
